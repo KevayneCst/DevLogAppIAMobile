509b2c997d2fadc01ad22a19dd23a87f77a1a41a
Nicolas Perrin <Nicolas.perrin83110@gmail.com>
2023-01-04 11:06:55 +0100

    added modifs for storing acc_gyr values to csv (working)

--- app/src/main/java/com/ankk/motiontracker/MainActivity.java
+++ app/src/main/java/com/ankk/motiontracker/MainActivity.java
@@ -1,95 +1,183 @@
 package com.ankk.motiontracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.os.Environment;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private SensorManager sensorManager;
-    private TextView accValText = null;
-    private TextView gyrValText = null;
-    private AccelerometerValues accVal = new AccelerometerValues(0, 0, 0);
-    private GyroscopeValues gyrVal = new GyroscopeValues(0, 0, 0);
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Button startButton;
+    private Button stopButton;
+    private Button runButton;
+    private Button walkButton;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private Sensor gyroscope;
+
+    private FileWriter writer;
+    private boolean recording;
+    private String currentActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        startButton = findViewById(R.id.start_button);
+        stopButton = findViewById(R.id.stop_button);
+        runButton = findViewById(R.id.run_button);
+        walkButton = findViewById(R.id.walk_button);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Sensor gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-        sensorManager.registerListener(accListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(gyrListener, gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startRecording();
+            }
+        });
 
-        //This is in the onCreate method
-        LinearLayout lView = new LinearLayout(this);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopRecording();
+            }
+        });
 
-        accValText = new TextView(this);
-        gyrValText = new TextView(this);
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentActivity = "Run";
+            }
+        });
 
-        lView.addView(accValText);
-        lView.addView(gyrValText);
+        walkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentActivity = "Walk";
+            }
+        });
 
-        setContentView(lView);
+        //in order to ask the permissions whatever
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (sensorManager != null) {
-            sensorManager.unregisterListener(accListener);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // Permission granted
+        } else {
+            // Permission denied
         }
     }
 
-    //Accelerometer Listener
-    private SensorEventListener accListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            // The acceleration may be negative, so take their absolute value
-            float xAccValue = Math.abs(event.values[0]);
-            float yAccValue = Math.abs(event.values[1]);
-            float zAccValue = Math.abs(event.values[2]);
+    private void startRecording() {
+        if (!recording) {
+            try {
+                File dir = new File(Environment.getExternalStorageDirectory(), "sensor_data");
+                if (!dir.exists()) {
+                    dir.mkdir();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
+                String currentDateAndTime = sdf.format(new Date());
+                File file = new File(dir, "data_" +currentDateAndTime + ".csv");
+                writer = new FileWriter(file);
+                writer.append("Timestamp,Acc_X,Acc_Y,Acc_Z,Gyr_X,Gyr_Y,Gyr_Z,Activity\n");
+                recording = true;
 
-            accVal.setX(xAccValue);
-            accVal.setY(yAccValue);
-            accVal.setZ(zAccValue);
+                String filePath = file.getAbsolutePath();
+                Toast.makeText(MainActivity.this, "Started recording sensor data in file : " + filePath, Toast.LENGTH_SHORT).show();
 
-            accValText.setText(accVal.toString());
-          }
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    };
+    }
 
-    //Gyroscope Listener
-    private SensorEventListener gyrListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            // The acceleration may be negative, so take their absolute value
-            float xGyrValue = Math.abs(event.values[0]);
-            float yGyrValue = Math.abs(event.values[1]);
-            float zGyrValue = Math.abs(event.values[2]);
-
-            gyrVal.setX(xGyrValue);
-            gyrVal.setY(yGyrValue);
-            gyrVal.setZ(zGyrValue);
-
-            gyrValText.setText(gyrVal.toString());
-
-         }
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    private void stopRecording() {
+        if (recording) {
+            try {
+                writer.flush();
+                writer.close();
+                recording = false;
+                Toast.makeText(MainActivity.this, "Stopped recording sensor data", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    };
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (recording) {
+            try {
+                long timestamp = event.timestamp;
+                float acc_x = event.values[0];
+                float acc_y = event.values[1];
+                float acc_z = event.values[2];
+                float gyr_x = 0.0f;
+                float gyr_y = 0.0f;
+                float gyr_z = 0.0f;
+                if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                    gyr_x = event.values[0];
+                    gyr_y = event.values[1];
+                    gyr_z = event.values[2];
+                }
+                writer.append(timestamp + "," + acc_x + "," + acc_y + "," + acc_z + "," + gyr_x + "," + gyr_y + "," + gyr_z + "," + currentActivity + "\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not used
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 }
+
