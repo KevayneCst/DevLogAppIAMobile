ede7493d5590ebf5dc870a79cb94221226f4510f
Nicolas Perrin <Nicolas.perrin83110@gmail.com>
2023-01-25 11:34:52 +0100

    Split the code with a Service to get data in background

--- app/src/main/java/com/ankk/motiontracker/MainActivity.java
+++ app/src/main/java/com/ankk/motiontracker/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat;
 
 
 import android.Manifest;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -22,6 +23,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 //import android.support.v7.app.AppCompatActivity;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,7 +49,7 @@ import java.util.Locale;
 
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
     private Button stopButton;
@@ -58,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private TextView predText;
 
+    /*
     private SensorManager sensorManager;
     private Sensor accelerometer;
     private Sensor gyroscope;
@@ -66,16 +69,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private FileWriter writer_pred;
     private boolean recording;
     private int currentActivity;
-
-    private PrepareDataTask dataprocess;
-
+*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        Intent intentDashboard = new Intent(this,DashboardActivity.class);
+
+        Intent intentDashboard = new Intent(this, DashboardActivity.class);
 
 
         /**
@@ -91,47 +92,57 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         predText = findViewById(R.id.result_text);
 
+        /*
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+*/
 
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startRecording();
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                startService(serviceIntent);
             }
         });
 
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopRecording();
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                stopService(serviceIntent);
             }
         });
 
         runButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentActivity = 2;
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                serviceIntent.putExtra("currentActivity", 2);
+                startService(serviceIntent);
             }
         });
 
         walkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentActivity = 1;
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                serviceIntent.putExtra("currentActivity", 1);
+                startService(serviceIntent);
             }
         });
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                currentActivity = 0;
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                serviceIntent.putExtra("currentActivity", 0);
             }
         });
         processButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopRecording();
+                Intent serviceIntent = new Intent(MainActivity.this, MySensorService.class);
+                stopService(serviceIntent);
                 startActivity(intentDashboard);
             }
         });
@@ -141,157 +152,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
         }
-
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            // Permission granted
-        } else {
-            // Permission denied
-        }
-    }
-
-    private void startRecording() {
-        if (!recording) {
-            try {
-                File dir = new File(Environment.getExternalStorageDirectory(), "sensor_data");
-                if (!dir.exists()) {
-                    dir.mkdir();
-                }
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
-                String currentDateAndTime = sdf.format(new Date());
-                File file = new File(dir, "data_" +currentDateAndTime + ".csv");
-                File predFile = new File(dir,"prediction_file.csv");
-                writer = new FileWriter(file);
-                writer.append("Timestamp,Acc_X,Acc_Y,Acc_Z,Gyr_X,Gyr_Y,Gyr_Z,Activity\n");
-
-                writer_pred = new FileWriter(predFile);
-                writer_pred.append("Timestamp,Acc_X,Acc_Y,Acc_Z,Gyr_X,Gyr_Y,Gyr_Z,Activity\n");
-                recording = true;
-
-                String filePath = file.getAbsolutePath();
-                Toast.makeText(MainActivity.this, "Started recording sensor data in file : " + filePath, Toast.LENGTH_SHORT).show();
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void stopRecording() {
-        if (recording) {
-            try {
-                writer.flush();
-                writer.close();
-                writer_pred.flush();
-                writer_pred.close();
-                recording = false;
-                Toast.makeText(MainActivity.this, "Stopped recording sensor data", Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        PrepareDataTask dataprocess = new PrepareDataTask(MainActivity.this,10);
-        dataprocess.execute();
-        //Toast.makeText(MainActivity.this, "Record Size : " + x.length, Toast.LENGTH_SHORT).show();
-
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (recording) {
-            try {
-                long timestamp = event.timestamp;
-                float acc_x = event.values[0];
-                float acc_y = event.values[1];
-                float acc_z = event.values[2];
-                float gyr_x = 0.0f;
-                float gyr_y = 0.0f;
-                float gyr_z = 0.0f;
-                if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-                    gyr_x = event.values[0];
-                    gyr_y = event.values[1];
-                    gyr_z = event.values[2];
-                }
-                writer.append(timestamp + "," + acc_x + "," + acc_y + "," + acc_z + "," + gyr_x + "," + gyr_y + "," + gyr_z + "," + currentActivity + "\n");
-                writer_pred.append(timestamp + "," + acc_x + "," + acc_y + "," + acc_z + "," + gyr_x + "," + gyr_y + "," + gyr_z + "\n");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Not used
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(this);
-    }
-    private class PrepareDataTask extends AsyncTask<Void, Void, float[][][]> {
-        private Context context;
-        private int n_past;
-
-        public PrepareDataTask(Context context, int n_past) {
-            this.context = context;
-            this.n_past = n_past;
-        }
-        @Override
-        protected float[][][] doInBackground(Void... voids) {
-            try {
-                // Load the data from the CSV file
-                FileReader fileReader = new FileReader(Environment.getExternalStorageDirectory() + "/sensor_data/prediction_file.csv");
-                CSVReader csvReader = new CSVReader(fileReader);
-
-                List<String[]> allRows = csvReader.readAll();
-                int n = allRows.size();
-                float[][][] data = new float[n][n_past][6];
-
-                for (int i = this.n_past+1; i < n; i++) {
-                    for (int j=i-this.n_past;j<i;j++){
-                        String[] row = allRows.get(j);
-                        for (int k = 0; k < 6; k++) {
-                            data[j][j+this.n_past-i][k] = Float.parseFloat(row[k+1]);
-                        }
-                    }
-                }
-                float[][] inputArray = data[10];
-                ActivityClassifier activityClassifier = new ActivityClassifier(MainActivity.this);
-                ActivityClassifier.ActivityType pred = activityClassifier.classifyActivity(inputArray);
-                System.out.println(pred);
-                predText.setText(pred.toString());
-
-
-                return data;
-            } catch (Exception e) {
-                Log.e(TAG, "Error reading CSV file", e);
-                return null;
-            }
-        }
-        private MappedByteBuffer loadModelFile(Context context) throws IOException {
-            AssetFileDescriptor fileDescriptor = context.getAssets().openFd("model.tflite");
-            FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-            FileChannel fileChannel = inputStream.getChannel();
-            long startOffset = fileDescriptor.getStartOffset();
-            long declaredLength = fileDescriptor.getDeclaredLength();
-            return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-        }
-    }
 }
 
---
+++ app/src/main/java/com/ankk/motiontracker/MySensorService.java
@@ -0,0 +1,143 @@
+package com.ankk.motiontracker;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MySensorService extends Service implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private FileWriter writer;
+    private boolean recording = false;
+
+    @Override
+    public void onCreate() {
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if(intent.getExtras()!=null){
+            int currentActivity = intent.getExtras().getInt("currentActivity");
+            startRecording(currentActivity);
+        }else {
+            startRecording();
+        }
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopRecording();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // Enregistrement des valeurs du capteur dans un fichier CSV
+        try {
+            writer.append(String.valueOf(event.timestamp));
+            writer.append(",");
+            for (float val : event.values) {
+                writer.append(String.valueOf(val));
+                writer.append(",");
+            }
+            writer.append("\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    private void startRecording(int currentActivity) {
+        if (!recording) {
+            try {
+                File dir = new File(Environment.getExternalStorageDirectory(), "sensor_data");
+                if (!dir.exists()) {
+                    dir.mkdir();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
+                String currentDateAndTime = sdf.format(new Date());
+                File file = new File(dir, "data_" + currentDateAndTime + ".csv");
+                writer = new FileWriter(file);
+                writer.append("Timestamp,Acc_X,Acc_Y,Acc_Z,Activity\n");
+                recording = true;
+                writer.append(String.valueOf(currentActivity));
+                writer.append(",");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startRecording() {
+        if (!recording) {
+            try {
+                File dir = new File(Environment.getExternalStorageDirectory(), "sensor_data");
+                if (!dir.exists()) {
+                    dir.mkdir();
+                }
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.US);
+                String currentDateAndTime = sdf.format(new Date());
+                File file = new File(dir, "data_" + currentDateAndTime + ".csv");
+                writer = new FileWriter(file);
+                writer.append("Timestamp,Acc_X,Acc_Y,Acc_Z,Activity\n");
+                recording = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopRecording() {
+        if (recording) {
+            try {
+                writer.flush();
+                writer.close();
+                recording = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
